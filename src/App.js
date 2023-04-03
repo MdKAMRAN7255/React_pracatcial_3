a0f8eae5ca08ae2ae074c12a2ab15969b6d3fa12
Md Kamran <mdkamran7255@gmail.com>
2023-04-03 20:03:07 +0530

    Completed Todo App Practical

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './component/main/main';
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  return (
+    <div className="App">
+     <Main />
+    </div>
+  );
+}
+
+export default App;
